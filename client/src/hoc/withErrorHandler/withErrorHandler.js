ac49bd69b115d9ad14b4650e3559cc2d79d3328b
Athura <joshualjohnson33@gmail.com>
2019-01-27 15:05:54 -0500

    Finished axios section, added componentWillUnmount to withErrorHandler

--- client/src/hoc/withErrorHandler/withErrorHandler.js
+++ client/src/hoc/withErrorHandler/withErrorHandler.js
@@ -12,18 +12,26 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
         }
 
         componentWillMount() {
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({
                     error: null
                 })
                 return req;
             })
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 this.setState({
                     error: error
                 })
             })
         }
+
+        // We eject the interceptors here because when the current component gets
+        // unmounted we don't want the above interceptors to still be mounted. That will cause a memory leak
+        componentWillUnmount = () => {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
+        }
+
 
         errorConfirmHandler = () => {
             this.setState({
